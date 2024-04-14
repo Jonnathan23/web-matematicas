8900f7832155d5c506226c37ccdc8bdb21b1a7e8
Jonnathan <jsaquicela26@gmail.com>
2024-04-14 11:51:31 -0500

    Ocultar y mostrar los ejemplos

--- javascript/ocultarEj.js
+++ javascript/ocultarEj.js
@@ -1,3 +1,8 @@
+//* Cick
+const verSumar = document.querySelector('#click-sumar');
+const verRestar = document.querySelector('#click-restar');
+const verMulti = document.querySelector('#click-multi');
+const verDiv = document.querySelector('#click-div');
 
 //* Ejemplos
 const contSumar = document.querySelector('#cont-sumar');
@@ -11,12 +16,25 @@ const imgRestar = document.querySelector('#resta');
 const imgMulti = document.querySelector('#multiplicacion');
 const imgDiv = document.querySelector('#division');
 
-console.log(imgSumar);
-console.log(imgRestar);
-console.log(imgMulti);
-console.log(imgDiv);
+console.log(verSumar);
+console.log(verRestar);
+console.log(verMulti);
+console.log(verDiv);
 
-contSumar.addEventListener('click', e => imgSumar.classList.toggle('mostrar'));
-contRestar.addEventListener('click', e => imgRestar.classList.toggle('mostrar'));
-contMulti.addEventListener('click', e => imgMulti.classList.toggle('mostrar'));
-contDiv.addEventListener('click', e => imgDiv.classList.toggle('mostrar'));
+contSumar.addEventListener('click', e => {
+    verSumar.classList.toggle('ocultar');
+    imgSumar.classList.toggle('mostrar');
+});
+
+contRestar.addEventListener('click', e => {
+    verRestar.classList.toggle('ocultar');
+    imgRestar.classList.toggle('mostrar');
+});
+contMulti.addEventListener('click', e => {
+    verMulti.classList.toggle('ocultar');
+    imgMulti.classList.toggle('mostrar');
+});
+contDiv.addEventListener('click', e => {
+    verDiv.classList.toggle('ocultar');
+    imgDiv.classList.toggle('mostrar');
+});
